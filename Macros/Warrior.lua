d46a8be7c319840e12cc9e94fe2b91d1c1a9ac1c
Timothy Minahan <timothy@minahan.net>
2016-09-14 10:21:15 +0930

    Adding HP_BladeFury Set of Macros from WLM Toxicdust

--- Macros/Warrior.lua
+++ Macros/Warrior.lua
@@ -3,3 +3,66 @@ local Sequences = GSMasterSequences
 ------------------
 ----- Warrior
 ------------------
+Sequences['HP_BladeFuryBuilder'] = {
+author="Toxicdust",
+specID=72,
+helpTxt = "Talents: 2332333",
+StepFunction = GSStaticPriority,
+icon='INV_MISC_QUESTIONMARK',
+lang="enUS",
+PreMacro=[[
+/cast [combat] Battle Cry
+/cast [combat] Blood Fury
+/cast [combat] Avatar
+/targetenemy [noharm][dead]
+]],
+"/castsequence Bloodthirst, Raging Blow, Furious Slash",
+PostMacro=[[
+/use [combat] 13
+/use [combat] 14
+]],
+}
+
+Sequences['HP_BladeFuryDump'] = {
+author="Toxicdust",
+specID=72,
+helpTxt = "Talents: 2332333",
+StepFunction = GSStaticPriority,
+icon='INV_MISC_QUESTIONMARK',
+lang="enUS",
+PreMacro=[[
+/cast [combat] Battle Cry
+/cast [combat] Blood Fury
+/cast [combat] Avatar
+/targetenemy [noharm][dead]
+]],
+"/cast [combat] Dragon Roar",
+"/cast Rampage",
+"/cast Execute",
+PostMacro=[[
+/use [combat] 13
+/use [combat] 14
+]],
+}
+
+Sequences['HP_BladeFuryAE'] = {
+author="Toxicdust",
+specID=72,
+helpTxt = "Talents: 2332333",
+StepFunction = GSStaticPriority,
+icon='INV_MISC_QUESTIONMARK',
+lang="enUS",
+PreMacro=[[
+/cast [combat] Battle Cry
+/cast [combat] Blood Fury
+/cast [combat] Avatar
+/targetenemy [noharm][dead]
+]],
+"/cast Dragon Roar",
+"/cast Odyn's Fury",
+"/cast Whirlwind",
+PostMacro=[[
+/use [combat] 13
+/use [combat] 14
+]],
+}

58370357a695cc2af47ee98edfcfb4fd33545be9
Timothy Minahan <timothy@minahan.net>
2016-09-20 07:59:23 +0930

    Hunter updates from https://wowlazymacros.com/forums/topic/my-improved-gs-e-leveling-raiding-or-high-performance-macros/#post-33652

--- Macros/Hunter.lua
+++ Macros/Hunter.lua
@@ -31,12 +31,9 @@ PostMacro = [[
 Sequences['HP_RBMmain'] = {
 specID = 253,
 author = "Sir_Ewing",
-helpTxt = "Raiding main - Talent: 3212233",
-StepFunction = GSStaticPriority,
+helpTxt = "Raiding main With Barrage- Talent: 3212223",
 PreMacro = [[
-/targetenemy [noharm][dead]
-/startattack
-/petattack [@target,harm]
+/cast [@pet,dead]Heart of the Phoenix
 /petautocastoff [group] Growl
 /petautocaston [nogroup] Growl
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
@@ -44,6 +41,7 @@ PreMacro = [[
 "/cast [nochanneling] Bestial Wrath",
 "/cast [nochanneling] !Kill Command",
 "/cast [nochanneling] Dire Frenzy",
+"/cast Barrage",
 "/cast [nochanneling] Concussive Shot",
 "/cast [nochanneling] Cobra Shot",
 "/cast [nochanneling] Titan's Thunder",
@@ -58,8 +56,6 @@ Sequences['HP_lookdead'] = {
 specID = 253,
 author = "Sir_Ewing",
 helpTxt = "Look Dead - Talent: 3222321",
-icon = "Ability_Mage_TormentOfTheWeak",
-StepFunction = GSStaticPriority,
 "/cast [nochanneling] Feign Death",
 "/cast [nochanneling] Play Dead",
 }
@@ -68,8 +64,6 @@ Sequences['HP_OShit'] = {
 specID = 253,
 author = "Sir_Ewing",
 helpTxt = "Oh Shit - Talent: 3222321",
-icon = "Ability_Hunter_MendPet",
-StepFunction = GSStaticPriority,
 "/cast Aspect of the Turtle",
 "/cast [target=player, help] Spirit Mend",
 "/cast [nochanneling] Exhilaration",
@@ -81,12 +75,8 @@ Sequences['HP_BMburst'] = {
 specID = 253,
 author = "Sir_Ewing",
 helpTxt = "BMAoE - Talent: 3222321",
-icon = "Ability_Hunter_KillCommand",
-StepFunction = GSStaticPriority,
 PreMacro = [[
-/targetenemy [noharm][dead]
-/startattack
-/petattack [@target,harm]
+/cast [@pet,dead]Heart of the Phoenix
 /petautocastoff [group] Growl
 /petautocaston [nogroup] Growl
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
@@ -104,9 +94,9 @@ PostMacro = [[
 
 Sequences['HP_Healpet'] = {
 specID = 253,
-author = "Sir_Ewing",
+author = "Sir-Ewing",
 helpTxt = "Pet Heal - Talent: 3222321",
-StepFunction = GSStaticPriority,
+"/cast [@pet,dead]Revive Pet",
 "/cast [nochanneling] !mend pet",
 "/cast [nochanneling] Exhilaration",
 }
@@ -115,11 +105,8 @@ Sequences['HP_BM2'] = {
 specID = 253,
 author = "Sir_Ewing",
 helpTxt = "Without Barrage - Talent: 3222321",
-StepFunction = GSStaticPriority,
 PreMacro = [[
-/targetenemy [noharm][dead]
-/startattack
-/petattack [@target,harm]
+/cast [@pet,dead]Heart of the Phoenix
 /petautocastoff [group] Growl
 /petautocaston [nogroup] Growl
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
@@ -142,11 +129,8 @@ Sequences['HP_BM1'] = {
 specID = 253,
 author = "Sir_Ewing",
 helpTxt = "With Barrage - Talent: 3222321",
-StepFunction = GSStaticPriority,
 PreMacro = [[
-/targetenemy [noharm][dead]
-/startattack
-/petattack [@target,harm]
+/cast [@pet,dead]Heart of the Phoenix
 /petautocastoff [group] Growl
 /petautocaston [nogroup] Growl
 /cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
@@ -156,6 +140,94 @@ PreMacro = [[
 "/cast [nochanneling] Intimidation",
 "/cast [nochanneling] Dire Frenzy",
 "/cast Barrage",
+"/cast [nochanneling] Titan's Thunder",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Bestial Wrath",
+}
+
+Sequences['HP_2RSBM'] = {
+specID = 253,
+author = "Sir_Ewing",
+helpTxt = "2nd Raiding Single without Barrage- Talent: 3212223",
+PreMacro = [[
+/cast [@pet,dead]Heart of the Phoenix
+/petautocastoff [group] Growl
+/petautocaston [nogroup] Growl
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
+]],
+"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Dire Frenzy",
+"/cast [nochanneling] Concussive Shot",
+"/cast [nochanneling] Cobra Shot",
+"/cast [nochanneling] Titan's Thunder",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Bestial Wrath",
+PostMacro = [[
+/cast Aspect of the Wild
+]],
+}
+
+Sequences['HP_2RBMAoE'] = {
+specID = 253,
+author = "Sir_Ewing",
+helpTxt = "2nd Raiding AoE with Barrage- Talent: 3212223",
+PreMacro = [[
+/cast [@pet,dead]Heart of the Phoenix
+/petautocastoff [group] Growl
+/petautocaston [nogroup] Growl
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
+]],
+"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Dire Frenzy",
+"/cast Barrage",
+"/cast [nochanneling] Multi-Shot",
+"/cast [nochanneling] Titan's Thunder",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Bestial Wrath",
+PostMacro = [[
+/cast Aspect of the Wild
+]],
+}
+
+Sequences['HP_SBMmain'] = {
+specID = 253,
+author = "Sir_Ewing",
+helpTxt = "Solo main - Talent: 3212333",
+PreMacro = [[
+/cast [@pet,dead]Heart of the Phoenix
+/petautocastoff [group] Growl
+/petautocaston [nogroup] Growl
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
+]],
+"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Dire Frenzy",
+"/cast [nochanneling] Concussive Shot",
+"/cast [nochanneling] Cobra Shot",
+"/cast [nochanneling] Titan's Thunder",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Bestial Wrath",
+PostMacro = [[
+/cast Aspect of the Wild
+]],
+}
+
+Sequences['HP_SAoE'] = {
+specID = 253,
+author = "Sir_Ewing",
+helpTxt = "Raiding AoE - Talent: 3212233",
+PreMacro = [[
+/cast [@pet,dead]Heart of the Phoenix
+/petautocastoff [group] Growl
+/petautocaston [nogroup] Growl
+/cast [target=focus, exists, nodead],[target=pet, exists, nodead] Misdirection
+]],
+"/cast [nochanneling] Bestial Wrath",
+"/cast [nochanneling] !Kill Command",
+"/cast [nochanneling] Dire Frenzy",
+"/cast [nochanneling] Multi-Shot",
 "/cast [nochanneling] Titan's Thunder",
 "/cast [nochanneling] !Kill Command",
 "/cast [nochanneling] Bestial Wrath",

cdef73eabbc601b38f1e6ef73dfabdc6cbad8f21
EnixLHQ <enixlhq@gmail.com>
2016-10-14 11:00:26 -0700

    Update DeathKnight.lua
    Updated macro

--- Macros/DeathKnight.lua
+++ Macros/DeathKnight.lua
@@ -6,19 +6,20 @@ local Sequences = GSMasterSequences
 Sequences['HP_EX_Unholy'] = {
 author="Dohram and EnixLHQ",
 specID=252,
-helpTxt = "Talents: 122??13 See: https://wowlazymacros.com/forums/topic/unholy-soul-reaper/",
+version=10,
+source = "Local",
+helpTxt = "Talents: 122??33. Hold ALT for Death Strike.",
 icon='INV_MISC_QUESTIONMARK',
 lang="enUS",
 PreMacro=[[
 /targetenemy [noharm][dead]
 /cast [nopet,nomod] Raise Dead
-/cast [combat] Anti-Magic Shell
 /use [mod:alt] Death Strike
 /castsequence  reset=combat  Outbreak, Festering Strike, Festering Strike, null
 ]],
 "/cast Dark Transformation",
-"/castsequence  reset=target  Festering Strike, Festering Strike, Scourge Strike, Scourge Strike, Scourge Strike",
-"/castsequence  reset=target  Outbreak, Festering Strike, Festering Strike, Soul Reaper",
+"/castsequence  reset=target  Festering Strike, Festering Strike, Scourge Strike, Scourge Strike",
+"/castsequence  reset=target  Outbreak, Festering Strike, Festering Strike, Soul Reaper, Scourge Strike, Scourge Strike",
 "/cast Apocalypse",
 "/cast Summon Gargoyle",
 "/cast Death Coil",

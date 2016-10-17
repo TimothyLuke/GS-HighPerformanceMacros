571ed34097dddc1b3427dd31618430bfe76c138d
Timothy Minahan <timothy@minahan.net>
2016-10-18 08:02:22 +1030

    more 7.1 mods

--- loaded.lua
+++ loaded.lua
@@ -1,6 +1,9 @@
 local GNOME, Sequences = ...
 local modversion = GetAddOnMetadata(GNOME, "Version")
 local AceEvent = LibStub("AceEvent-3.0")
+GSWLMHPOptions = {}
+GSWLMHPOptions.warnupdate = false
+GSWLMHPOptions.currentversion = modversion
 
 GSPrint("HP Macros loaded.  This set currently contains macros for ", GNOME)
 GSPrint("  - Enhancement Shaman ", GNOME)
@@ -17,18 +20,19 @@ end
 GSMasterSequences = Sequences
 
 local function processAddonLoaded()
-  print("Loaded")
-  for k_v in pairs(KnownSequences) do
-    if GSMasterOptions.SequenceLibrary[k][GSGetActiveSequenceVersion(k).source] ~= GNOME then
+  for k,_ in pairs(KnownSequences) do
+    if GSMasterOptions.SequenceLibrary[k][GSGetActiveSequenceVersion(k)].source ~= GNOME then
       GSPrint("You have made edits to " .. k .. ".  If you have updated ".. GNOME .. " recently, there may be updates to version 1 of this macro.", GNOME)
-    elseif GSWLMHPOptions[k] ~= modversion and not GSWLMHPOptions[warnupdate] then
-      GSPrint("The author of " .. k .. " may have updated this macro. Please check your talents still match.", GNOME)
-      GSWLMHPOptions[k] = modversion
-      GSWLMHPOptions[warnupdate] = true
-    else
-      GSWLMHPOptions[k] = modversion
     end
   end
+  if GSWLMHPOptions.currentversion ~= modversion  then
+    GSPrint("The HP Macros mod has been updated.  The following Macros may have been updated.  Please check your talents still match and any local modifications.", GNOME)
+    for k,_ in pairs(KnownSequences) do
+      GSPrint("  - ".. k, GNOME)
+    end
+    GSWLMHPOptions.currentversion = modversion
+  end
+
 end
 
 AceEvent:RegisterMessage(GSStaticCoreLoadedMessage,  processAddonLoaded)

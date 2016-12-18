f05784ac455f4f1d1aba95e34e9a9c89002b3d8b
Timothy Minahan <timothy@minahan.net>
2016-12-19 09:00:20 +1030

    Add EnixLHQ Demonology Warlock

--- Macros/Warlock.lua
+++ Macros/Warlock.lua
@@ -2,3 +2,42 @@ local GNOME, Sequences = ...
 ------------------
 ----- Warlock
 ------------------
+
+Sequences['HP_EX_Demo'] = {
+author='EnixLHQ',
+specID=266,
+helpTxt = 'Talents: 3211232 - https://wowlazymacros.com/forums/topic/demonology-synergy/ - This macro tries to hit the targets every Demonologist should be aiming for: Soul Shard generation for maximum Wild Imps uptime, DreadStalkers on CD, and plenty of Demonic Empowerment.',
+icon='INV_MISC_QUESTIONMARK',
+PreMacro=[[
+/targetenemy [noharm][dead]
+/cast [nopet][target=pet, dead] Summon Felguard
+/use [mod:alt] Drain Life
+/castsequence  reset=target  Doom, null
+]],
+"/castsequence [nochanneling] Demonbolt, Call Dreadstalkers, Demonic Empowerment, Life Tap",
+"/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Demonic Empowerment",
+"/castsequence [nochanneling] Summon Doomguard, Demonic Empowerment",
+"/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Thal'kiel's Consumption",
+"/cast [nochanneling] Command Demon",
+"/cast [nochanneling] Demonbolt",
+PostMacro=[[
+]],
+}
+
+Sequences['HP_EX_DemoAoE'] = {
+author='EnixLHQ',
+specID=266,
+helpTxt = 'Talents: 3211232 - https://wowlazymacros.com/forums/topic/demonology-synergy/ - This macro tries to hit the targets every Demonologist should be aiming for: Soul Shard generation for maximum Wild Imps uptime, DreadStalkers on CD, and plenty of Demonic Empowerment.',
+icon='INV_MISC_QUESTIONMARK',
+PreMacro=[[
+/targetenemy [noharm][dead]
+/cast [nopet][target=pet, dead] Summon Felguard
+/use [mod:alt] Drain Life
+/castsequence  reset=target  Doom, null
+]],
+"/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath",
+"/castsequence [nochanneling] Call Dreadstalkers, Demonic Empowerment, Life Tap",
+"/cast [nochanneling] Command Demon",
+PostMacro=[[
+]],
+}
--- loaded.lua
+++ loaded.lua
@@ -19,6 +19,7 @@ local function processAddonLoaded(event, arg)
     GSE.Print("  - Fury Warriors", ModName)
     GSE.Print("  - Beast Master Hunters", ModName)
     GSE.Print("  - Unholy , Blood and Frost Death Knights", ModName)
+    GSE.Print("  - Demonology Warlock", ModName)
     GSE.Print("You can find help and also nominate other macros for this set at https://www.wowlazymacros.com", ModName)
   end
 end

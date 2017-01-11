bb2afb892be3ed229be513c13029f6009d3ed01a
EnixLHQ <brian.r.luedtke@gmail.com>
2017-01-11 09:20:27 -0800

    Updated for 7.1.5
    Took care not to mess with your formatting. Also took the liberty of adding Affliction (don't recall if it was nominated or accepted, and if in conflict please delete).

--- Macros/Warlock.lua
+++ Macros/Warlock.lua
@@ -3,17 +3,41 @@ local _, Sequences = ...
 ----- Warlock
 ------------------
 
-Sequences['HP_EX_Demo'] = {
+Sequences['HP_EX_Aff_715'] = {
   Author='EnixLHQ',
-  SpecID=266,
-  Help='Single Target - This macro tries to hit the targets every Demonologist should be aiming for: Soul Shard generation for maximum Wild Imps uptime, DreadStalkers on CD, and plenty of Demonic Empowerment.',
-  Helplink="https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Talents="3,2,1,1,2,3,2",
+  SpecID=265,
+  Help="Run at 80ms",
+  Helplink="https://wowlazymacros.com/forums/topic/affliction-singularity/",
+  Talents="3,1,?,2,?,1,3",
   Icon='INV_MISC_QUESTIONMARK',
   Default=1,
   MacroVersions = {
     [1] = {
       KeyPress={
+        "/targetenemy [noharm][dead]",
+        "/use [mod:alt] Drain Soul",
+        "/castsequence  reset=target  Agony, Corruption, null",
+      },
+      "/cast [nochanneling] Unstable Affliction",
+      "/castsequence [nochanneling] Unstable Affliction, Reap Souls",
+      "/castsequence [nochanneling] reset=combat  Agony, Corruption, Drain Soul, Life Tap",
+    }
+  }
+}
+
+Sequences['HP_EX_Demo_715'] = {
+  Author='EnixLHQ',
+  SpecID=266,
+  Help="Run at 80ms",
+  Helplink="https://wowlazymacros.com/forums/topic/demonology-synergy/",
+  Talents="3,2,?,1,?,2,2",
+  Icon='INV_MISC_QUESTIONMARK',
+  Default=1,
+  MacroVersions = {
+    [1] = {
+      KeyPress={
+        "/petautocastoff [group] Threatening Presence"
+        "/petautocaston [nogroup] Threatening Presence"
         "/targetenemy [noharm][dead]",
         "/cast [nopet][target=pet, dead] Summon Felguard",
         "/use [mod:alt] Drain Life",
@@ -23,30 +47,33 @@ Sequences['HP_EX_Demo'] = {
       "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Demonic Empowerment",
       "/castsequence [nochanneling] Summon Doomguard, Demonic Empowerment",
       "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Thal'kiel's Consumption",
+      "/cast [nochanneling] Grimoire: Felguard",
       "/cast [nochanneling] Command Demon",
       "/cast [nochanneling] Demonbolt",
     }
   }
 }
 
-Sequences['HP_EX_DemoAoE'] = {
+Sequences['HP_EX_DemoAoE_715'] = {
   Author='EnixLHQ',
   SpecID=266,
   Help = 'AOE - This macro tries to hit the targets every Demonologist should be aiming for: Soul Shard generation for maximum Wild Imps uptime, DreadStalkers on CD, and plenty of Demonic Empowerment.',
   Helplink="https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Talents="3,2,1,1,2,3,2",
+  Talents="3,2,?,1,?,2,2",
   Icon='INV_MISC_QUESTIONMARK',
   Default=1,
   MacroVersions = {
     [1] = {
       KeyPress={
+        "/petautocastoff [group] Threatening Presence"
+        "/petautocaston [nogroup] Threatening Presence"
         "/targetenemy [noharm][dead]",
         "/cast [nopet][target=pet, dead] Summon Felguard",
         "/use [mod:alt] Drain Life",
-        "/castsequence  reset=target  Doom, null",
+        "/castsequence  reset=target  Doom, null"
       },
-      "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath",
-      "/castsequence [nochanneling] Call Dreadstalkers, Demonic Empowerment, Life Tap",
+      "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath, Demonwrath, Life Tap",
+      "/cast [nochanneling] Grimoire: Felguard",
       "/cast [nochanneling] Command Demon",
     }
   }

1ae4c3d7bd2d126aa85e94bf8d48f384aed0904b
EnixLHQ <brian.r.luedtke@gmail.com>
2017-01-25 16:34:52 -0800

    Update Warlock.lua
    Updates from the forums.

--- Macros/Warlock.lua
+++ Macros/Warlock.lua
@@ -3,78 +3,133 @@ local _, Sequences = ...
 ----- Warlock
 ------------------
 
-Sequences['HP_EX_Aff_715'] = {
-  Author='EnixLHQ',
+Sequences['HP_EX_Aff_ST'] = {
+  Author="Exlynn@Perenolde with help from Belthozar@Pozzo dell’Eternità",  
   SpecID=265,
-  Help="Run at 80ms",
-  Helplink="https://wowlazymacros.com/forums/topic/affliction-singularity/",
-  Talents="3,1,?,2,?,1,3",
-  Icon='INV_MISC_QUESTIONMARK',
+  Talents = "3,1,?,1,?,1,3",
+  Helplink = "https://wowlazymacros.com/forums/topic/affliction-conduit-7-1-5/",
+  Help = "Single Target - Run at 80ms or manually",
   Default=1,
   MacroVersions = {
     [1] = {
+      StepFunction = [[Sequential]],
       KeyPress={
         "/targetenemy [noharm][dead]",
         "/use [mod:alt] Drain Soul",
-        "/castsequence  reset=target  Agony, Corruption, null",
+        "/castsequence  reset=target  Agony, Corruption, Siphon Life, null",
       },
-      "/cast [nochanneling] Unstable Affliction",
-      "/castsequence [nochanneling] Unstable Affliction, Reap Souls",
-      "/castsequence [nochanneling] reset=combat  Agony, Corruption, Drain Soul, Life Tap",
-    }
-  }
+      PreMacro={
+      },
+        "/castsequence [nochanneling] reset=combat/reset  Agony, Corruption, Siphon Life, Drain Soul, Life Tap",
+        "/cast [nochanneling] Unstable Affliction",
+        "/castsequence [nochanneling] Unstable Affliction, Drain Soul, Reap Souls",
+      PostMacro={
+      },
+      KeyRelease={
+        "/petautocastoff [group] Seethe",
+        "/petautocastoff [group] Burning Presence",
+        "/petautocaston [nogroup] Seethe",
+        "/petautocaston [nogroup] Burning Presence",
+      },
+    },
+  },
 }
 
-Sequences['HP_EX_Demo_715'] = {
-  Author='EnixLHQ',
-  SpecID=266,
-  Help="Run at 80ms",
-  Helplink="https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Talents="3,2,?,1,?,2,2",
-  Icon='INV_MISC_QUESTIONMARK',
+
+Sequences['HP_EX_Aff_AoE'] = {
+  Author="Exlynn@Perenolde",  
+  SpecID=265,
+  Talents = "3,1,?,1,?,1,3",
+  Helplink = "https://wowlazymacros.com/forums/topic/affliction-conduit-7-1-5/",
+  Help = "AoE - Run at 80ms or manually",
   Default=1,
   MacroVersions = {
     [1] = {
+      StepFunction = [[Sequential]],
       KeyPress={
-        "/petautocastoff [group] Threatening Presence",
-        "/petautocaston [nogroup] Threatening Presence",
         "/targetenemy [noharm][dead]",
-        "/cast [nopet][target=pet, dead] Summon Felguard",
-        "/use [mod:alt] Drain Life",
-        "/castsequence  reset=target  Doom, null"
+        "/use [mod:alt] Drain Soul",
+        "/castsequence  reset=target  Agony, Corruption, Siphon Life, null",
       },
-      "/castsequence [nochanneling] Demonbolt, Call Dreadstalkers, Demonic Empowerment, Life Tap",
-      "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Demonic Empowerment",
-      "/castsequence [nochanneling] Summon Doomguard, Demonic Empowerment",
-      "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Thal'kiel's Consumption",
-      "/cast [nochanneling] Grimoire: Felguard",
-      "/cast [nochanneling] Command Demon",
-      "/cast [nochanneling] Demonbolt",
-    }
-  }
+      PreMacro={
+      },
+        "/castsequence Seed of Corruption, Seed of Corruption, Seed of Corruption, Agony, Life Tap",
+        "/cast Reap Souls",
+      PostMacro={
+      },
+      KeyRelease={
+        "/petautocastoff [group] Seethe",
+        "/petautocastoff [group] Burning Presence",
+        "/petautocaston [nogroup] Seethe",
+        "/petautocaston [nogroup] Burning Presence",
+      },
+    },
+  },
 }
 
-Sequences['HP_EX_DemoAoE_715'] = {
-  Author='EnixLHQ',
+
+Sequences['HP_EX_Demo_ST'] = {
+  Author="Exlynn@Perenolde",  
   SpecID=266,
-  Help = "AOE Run at 80ms",
-  Helplink="https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Talents="3,2,?,1,?,2,2",
-  Icon='INV_MISC_QUESTIONMARK',
+  Talents = "3,1,?,1,?,2,2",
+  Helplink = "https://wowlazymacros.com/forums/topic/demonology-synergy/",
+  Help = "Single Target - Run at 80ms or manually",
   Default=1,
   MacroVersions = {
     [1] = {
+      StepFunction = [[Sequential]],
       KeyPress={
+        "/targetenemy [noharm][dead]",
+        "/use [mod:alt] Drain Life",
+        "/castsequence  reset=target  Doom, Demonbolt, null",
+      },
+      PreMacro={
+      },
+        "/castsequence [nochanneling] Demonbolt, Demonbolt, Hand of Gul'dan, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonbolt, Demonbolt, Call Dreadstalkers, Demonic Empowerment, Life Tap",
+        "/castsequence [nochanneling] Demonbolt, Demonbolt, Summon Doomguard, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonbolt, Demonbolt, Grimoire: Felguard, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonbolt, Demonbolt, Thal'kiel's Consumption",
+        "/cast [nochanneling] Command Demon",
+      PostMacro={
+      },
+      KeyRelease={
         "/petautocastoff [group] Threatening Presence",
         "/petautocaston [nogroup] Threatening Presence",
-        "/targetenemy [noharm][dead]",
-        "/cast [nopet][target=pet, dead] Summon Felguard",
-        "/use [mod:alt] Drain Life",
-        "/castsequence  reset=target  Doom, null"
       },
-      "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath, Demonwrath, Life Tap",
-      "/cast [nochanneling] Grimoire: Felguard",
-      "/cast [nochanneling] Command Demon",
-    }
-  }
+    },
+  },
 }
+
+
+Sequences['HP_EX_Demo_AoE'] = {
+  Author="Exlynn@Perenolde",  
+  SpecID=266,
+  Talents = "3,2,?,1,?,2,2",
+  Helplink = "https://wowlazymacros.com/forums/topic/demonology-synergy/",
+  Help = "AoE - Run at 80ms",
+  Default=1,
+  Icon='INV_MISC_QUESTIONMARK',
+  MacroVersions = {
+    [1] = {
+      StepFunction = [[Sequential]],
+      KeyPress={
+        "/targetenemy [noharm][dead]",
+        "/use [mod:alt] Drain Life",
+        "/castsequence  reset=target  Doom, null",
+      },
+      PreMacro={
+      },
+        "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath, Demonwrath, Life Tap",
+        "/cast [nochanneling] Grimoire: Felguard",
+        "/cast [nochanneling] Command Demon",
+      PostMacro={
+      },
+      KeyRelease={
+        "/petautocastoff [group] Threatening Presence",
+        "/petautocaston [nogroup] Threatening Presence",
+      },
+    },
+  },
+}
+

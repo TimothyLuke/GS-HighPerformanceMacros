ff35722ac88d7319f2af8a48193a940a412e4ac5
Timothy Minahan <timothy@minahan.net>
2017-02-09 13:53:29 +1030

    Updates from ToxicDust

--- Macros/Warrior.lua
+++ Macros/Warrior.lua
@@ -6,41 +6,49 @@ local _, Sequences = ...
 Sequences['HP_BladeFuryBuilder'] = {
   Author="Toxicdust",
   SpecID=72,
-  Help = "Talents: 2332333",
-  Talents="2,3,3,2,3,3,3",
-  StepFunction = "Priority",
-  Icon='INV_MISC_QUESTIONMARK',
+  Helplink = "https://wowlazymacros.com/forums/topic/hp-pve-fury-warrior-update-for-7-1-5/",
+  Talents = "2333132",
+  Help = "Talents have been setup to a best case scenario as per Icy Veins",
   Default=1,
+  Icon='INV_MISC_QUESTIONMARK',
   MacroVersions = {
     [1] = {
+      StepFunction = "Sequential",
       KeyPress={
         "/cast [combat] Battle Cry",
         "/cast [combat] Blood Fury",
         "/cast [combat] Avatar",
         "/targetenemy [noharm][dead]",
       },
-      "/castsequence Bloodthirst, Raging Blow, Furious Slash",
-    }
-  }
+      PreMacro={
+      },
+        "/castsequence Bloodthirst, Raging Blow, Furious Slash",
+      PostMacro={
+      },
+      KeyRelease={
+      },
+    },
+  },
 }
 
 Sequences['HP_BladeFuryDump'] = {
   Author="Toxicdust",
   SpecID=72,
-  Help="Talents: 2332333",
+  Helplink = "https://wowlazymacros.com/forums/topic/hp-pve-fury-warrior-update-for-7-1-5/",
   StepFunction="Priority",
-  Talents="2,3,3,2,3,3,3",
+  Talents = "2333132",
+  Help = "Talents have been setup to a best case scenario as per Icy Veins",
   Icon='INV_MISC_QUESTIONMARK',
   Default=1,
   MacroVersions = {
     [1] = {
+      StepFunction = "Sequential",
       KeyPress={
         "/cast [combat] Battle Cry",
         "/cast [combat] Blood Fury",
         "/cast [combat] Avatar",
         "/targetenemy [noharm][dead]",
       },
-      "/cast [combat] Dragon Roar",
       "/cast Rampage",
       "/cast Execute",
     }
@@ -50,23 +58,29 @@ Sequences['HP_BladeFuryDump'] = {
 Sequences['HP_BladeFuryAE'] = {
   Author="Toxicdust",
   SpecID=72,
-  Help = "Talents: 2332333",
-  Talents="2,3,3,2,3,3,3",
-  StepFunction = "Priority",
+  Talents = "2333132",
+  Help = "Talents have been setup to a best case scenario as per Icy Veins",
+  Helplink = "https://wowlazymacros.com/forums/topic/hp-pve-fury-warrior-update-for-7-1-5/",
+  Default=1,
   Icon='INV_MISC_QUESTIONMARK',
-  Default=1,
-  Default=1,
   MacroVersions = {
     [1] = {
+      StepFunction = "Sequential",
       KeyPress={
         "/cast [combat] Battle Cry",
         "/cast [combat] Blood Fury",
         "/cast [combat] Avatar",
         "/targetenemy [noharm][dead]",
       },
-      "/cast Dragon Roar",
-      "/cast Odyn's Fury",
-      "/cast Whirlwind",
-    }
-  }
+      PreMacro={
+      },
+        "/cast Odyn's Fury",
+        "/cast Whirlwind",
+        "/cast Rampage",
+      PostMacro={
+      },
+      KeyRelease={
+      },
+    },
+  },
 }

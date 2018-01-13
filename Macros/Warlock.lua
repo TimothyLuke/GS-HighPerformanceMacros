1089d675e87ba43d76c0b2c3f6ed7f7f95bbd7e9
EnixLHQ <brian.r.luedtke@gmail.com>
2018-01-13 14:50:46 -0800

    Update Warlock.lua
    All up-to-date

--- Macros/Warlock.lua
+++ Macros/Warlock.lua
@@ -3,27 +3,32 @@ local _, Sequences = ...
 ----- Warlock
 ------------------
 
-Sequences['HP_EX_Aff_ST'] = {
-  Author="Exlynn@Perenolde with help from Belthozar@Pozzo dell'Eternità",
+Sequences['HP_EX_Aff_Seed'] = {
+  Author="Exlynn@Perenolde",
   SpecID=265,
-  Talents = "3,2,?,1,?,1,3",
-  Helplink = "https://wowlazymacros.com/forums/topic/affliction-conduit-7-1-5/",
-  Help = [[Single Target - Run at 80ms, 100ms, or manually.]],
+  Talents = "3112213",
+  Help = [[Run at 80ms, 100ms, or manually.
+Hold ALT for Seed of Corruption
+Hold SHIFT Drain Soul]],
   Default=1,
   MacroVersions = {
     [1] = {
       StepFunction = "Sequential",
+      LoopLimit=8,
       KeyPress={
         "/targetenemy [noharm][dead]",
-        "/use [mod:alt] Drain Soul",
-        "/castsequence  reset=target  Corruption, Agony, Siphon Life, null",
+        "/petattack",
+        "/use [mod:alt] Seed of Corruption",
+        "/use [mod:shift] Drain Soul",
+        "/castsequence  reset=target  Agony, Corruption, null",
       },
       PreMacro={
+        "/use [nochanneling] Reap Souls",
+        "/use [nochanneling] Life Tap",
       },
         "/use [nopet,combat] Summon Doomguard",
-        "/castsequence [nochanneling] Agony, Siphon Life, Drain Soul, Life Tap, Agony",
-        "/cast [nochanneling] Unstable Affliction",
-        "/castsequence [nochanneling] Unstable Affliction, Drain Soul, Reap Souls",
+        "/castsequence [nochanneling] Agony, Corruption, Drain Soul, Agony",
+        "/castsequence [nochanneling] Unstable Affliction, Drain Soul",
       PostMacro={
       },
       KeyRelease={
@@ -35,65 +40,36 @@ Sequences['HP_EX_Aff_ST'] = {
     },
   },
 }
-
-
-Sequences['HP_EX_Aff_AoE'] = {
-  Author="Exlynn@Perenolde",
-  SpecID=265,
-  Talents = "3,2,?,1,?,1,3",
-  Helplink = "https://wowlazymacros.com/forums/topic/affliction-conduit-7-1-5/",
-  Help = "AoE - Run at 80ms, 100ms, or manually.",
-  Default=1,
-  MacroVersions = {
-    [1] = {
-      StepFunction = [[Sequential]],
-      KeyPress={
-        "/targetenemy [noharm][dead]",
-        "/use [mod:alt] Drain Soul",
-        "/castsequence  reset=target  Agony, Corruption, Siphon Life, null",
-      },
-      PreMacro={
-      },
-        "/use [nopet,combat] Summon Infernal",
-        "/castsequence Seed of Corruption, Seed of Corruption, Seed of Corruption, Agony, Life Tap",
-        "/cast Reap Souls",
-      PostMacro={
-      },
-      KeyRelease={
-        "/petautocastoff [group] Seethe",
-        "/petautocastoff [group] Burning Presence",
-        "/petautocaston [nogroup] Seethe",
-        "/petautocaston [nogroup] Burning Presence",
-      },
-    },
-  },
-}
-
 
 Sequences['HP_EX_Demo_ST'] = {
   Author="Exlynn@Perenolde",
   SpecID=266,
-  Talents = "3,1,?,1,?,2,2",
+  Talents = "3111223",
   Helplink = "https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Help = "Single Target - Run at 80ms or manually",
+  Help = [[Single Target - Run at 80ms, 100ms, or manually.
+Hold ALT for Drain Life
+Hold SHIFT Shadow Bolt/Demonbolt]],
   Default=1,
+  Icon='INV_MISC_QUESTIONMARK',
   MacroVersions = {
     [1] = {
-      StepFunction = [[Sequential]],
+      StepFunction = "Sequential",
+      LoopLimit=10,
       KeyPress={
         "/targetenemy [noharm][dead]",
         "/use [mod:alt] Drain Life",
+        "/use [mod:shift] Shadow Bolt",
         "/castsequence  reset=target  Doom, null",
       },
       PreMacro={
+        "/use Thal'kiel's Consumption",
+        "/use Life Tap",
       },
-        "/castsequence [nochanneling] Demonbolt, Call Dreadstalkers, Demonic Empowerment, Life Tap",
-        "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Demonic Empowerment",
-        "/castsequence [nochanneling] Summon Doomguard, Demonic Empowerment",
-        "/castsequence [nochanneling] Demonbolt, Hand of Gul'dan, Call Dreadstalkers, Thal'kiel's Consumption",
-        "/castsequence [nochanneling] Demonbolt, Demonbolt, Grimoire: Felguard, Demonic Empowerment",
-        "/cast [nochanneling] Command Demon",
-        "/cast [nochanneling] Demonbolt",
+        "/use [combat,nopet] Summon Felguard; Command Demon",
+        "/castsequence [nochanneling] Shadow Bolt, Call Dreadstalkers, Demonic Empowerment",
+        "/castsequence [nochanneling] Shadow Bolt, Shadow Bolt, Shadow Bolt, Hand of Gul'dan, Demonic Empowerment",
+        "/castsequence [nochanneling] Shadow Bolt, Summon Doomguard, Demonic Empowerment",
+        "/castsequence [nochanneling] Shadow Bolt, Grimoire: Felguard, Demonic Empowerment",
       PostMacro={
       },
       KeyRelease={
@@ -104,28 +80,34 @@ Sequences['HP_EX_Demo_ST'] = {
   },
 }
 
-
 Sequences['HP_EX_Demo_AoE'] = {
   Author="Exlynn@Perenolde",
   SpecID=266,
-  Talents = "3,1,?,1,?,2,2",
+  Talents = "3111223",
   Helplink = "https://wowlazymacros.com/forums/topic/demonology-synergy/",
-  Help = "AoE - Run at 80ms",
+  Help = [[AoE - Run at 80ms, 100ms, or manually.
+Hold ALT for Drain Life]],
   Default=1,
   Icon='INV_MISC_QUESTIONMARK',
   MacroVersions = {
     [1] = {
-      StepFunction = [[Sequential]],
+      StepFunction = "Sequential",
+      LoopLimit=10,
       KeyPress={
         "/targetenemy [noharm][dead]",
         "/use [mod:alt] Drain Life",
         "/castsequence  reset=target  Doom, null",
       },
       PreMacro={
+        "/use Thal'kiel's Consumption",
+        "/use Life Tap",
       },
-        "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment, Demonwrath, Demonwrath, Life Tap",
-        "/cast [nochanneling] Grimoire: Felguard",
-        "/cast [nochanneling] Command Demon",
+        "/use [combat,nopet] Summon Felguard; Command Demon",
+        "/castsequence [nochanneling] Demonwrath, Call Dreadstalkers, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonwrath, Hand of Gul'dan, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonwrath, Summon Infernal, Demonic Empowerment",
+        "/castsequence [nochanneling] Demonwrath, Grimoire: Felguard, Demonic Empowerment",
       PostMacro={
       },
       KeyRelease={
